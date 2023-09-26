9b877ce68d38c37cc14fc8fc60f7b7162efbcb3f
aman <amansagar262@gmail.com>
2023-09-26 14:05:45 +0530

    chore: use seperate function for svg markup

--- src/makeBlockiesUrl.ts
+++ src/makeBlockiesUrl.ts
@@ -4,15 +4,14 @@ const store: Record<string, string> = Object.create(null)
 
 const _btoa = btoa || window?.btoa || ((str: string) => Buffer.from(str, 'binary').toString('base64'))
 
-type ResultType = 'svg' | 'imageDataUrl'
+export default function makeBlockiesUrl(address: string, size = 8, caseSensitive = false, scale = 10) {
+	const svgMarkup = makeBlockiesSvgString(address, size, caseSensitive, scale)
+	const base64Url = `data:image/svg+xml;base64,${_btoa(svgMarkup)}`
+	store[`${size}:${address}`] = base64Url
+	return base64Url
+}
 
-export default function makeBlockiesUrl(
-	address: string,
-	type: ResultType = 'imageDataUrl',
-	size = 8,
-	caseSensitive = false,
-	scale = 10
-) {
+export function makeBlockiesSvgString(address: string, size = 8, caseSensitive = false, scale = 10) {
 	if (!address) throw new Error('Address is required')
 	if (!caseSensitive) address = address.toLowerCase()
 
@@ -53,11 +52,5 @@ export default function makeBlockiesUrl(
 					.join('')}
 			</g>
 		</svg>`
-
-	if (type === 'svg') return svgMarkup
-	const base64Url = `data:image/svg+xml;base64,${_btoa(svgMarkup)}`
-
-	store[`${size}:${address}`] = base64Url
-
-	return base64Url
+	return svgMarkup
 }
